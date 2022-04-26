e66d23ab2d7188fd940350219d65870130bc81e3
EvansMutwiri <mutwirievansm@gmail.com>
2022-04-27 00:21:57 +0300

    First Vue app

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <MyComponentVue/>
+</template>
+
+<script>
+import MyComponentVue from "./components/MyComponent.vue"
+export default {
+  data () {
+    return {
+      
+    }
+  },
+    components: {
+      MyComponentVue
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Hey I am {{ name }}</h1>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            name: 'Evans'
+        }
+    }
+}
+</script>
+
+<style>
+    h1 {
+        color: red;
+    }
+</style>
