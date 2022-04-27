59216125ebc1c43c579b3c49d58e45c18a6f3c89
EvansMutwiri <mutwirievansm@gmail.com>
2022-04-27 13:10:26 +0300

    demo of options api

---
+++ src/components/Options.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>{{name}}</h2>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            name : "Evans"
+        }
+    }, 
+    mounted () {
+        setTimeout(() => {
+            this.name = "Mutwiri";
+        }, 4000);
+    }
+}
+</script>
+
+<style>
+
+</style>
