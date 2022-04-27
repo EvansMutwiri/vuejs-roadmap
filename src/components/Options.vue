f0e0b414c19fab16c690b621b0a1ffb851d6a85e
EvansMutwiri <mutwirievansm@gmail.com>
2022-04-27 20:20:10 +0300

    applying reactive using ref

--- src/components/Options.vue
+++ src/components/Options.vue
@@ -1,23 +1,37 @@
 <template>
-<h1>PortFolio</h1>
-  <h2>First Name: {{firstName}}</h2>
-  <h2>Last Name: {{lastName}}</h2>
+    <div class="container">
+        <h1>PortFolio</h1>
+    <h2>First Name: {{ firstName }}</h2>
+    <h2>Last Name: {{ lastName }}</h2>
+    <hr>
+    <div>
+        <p> Contacts: {{ details.contact }}</p>
+        <p> Location: {{ details.address }}</p>
+    </div>
+    </div>
 </template>
 
 <script>
 import { ref } from '@vue/reactivity';
 export default {
-    setup () {
+    setup() {
         let first = ref('Evans');
         let last = ref('Mutwiri');
 
+        let details = ref({
+            contact: '0722000000',
+            address: 'Nairobi'
+        });
+
         setTimeout(() => {
-            first.value = 'Mutwiri',
-            last.value = 'Evans'
-        }, 4000);
+            details.value.address = 'Meru',
+            details.value.contact = '0721212121'
+        }, 4000)
+
         return {
-            firstName : first,
-            lastName : last
+            firstName: first,
+            lastName: last,
+            details: details
         }
     }
 }
@@ -31,10 +45,20 @@ body {
     align-items: center;
 }
 
+.container {
+    border: 1px none;
+    padding: 6em;
+    border-radius: 10px;
+    background: #d2cdc3;
+    box-shadow: 2px 2px 0 0 #938f88;
+    margin-top: 10%;
+}
+
 h1 {
-        /* font: 800 'Verdana'; */
-        font: 40px Georgia, serif;
-    }
+    /* font: 800 'Verdana'; */
+    font: 50px Georgia, serif 800;
+}
+
 h2 {
     font: 20px Arial, sans-serif;
 }
