97cb36db64f5e1c6d9af654443677ccc8cddb3a5
EvansMutwiri <mutwirievansm@gmail.com>
2022-04-27 16:11:04 +0300

    sample code demonstrating use of composition api and text interpolation

--- src/components/Options.vue
+++ src/components/Options.vue
@@ -1,22 +1,41 @@
 <template>
-  <h2>{{name}}</h2>
+<h1>PortFolio</h1>
+  <h2>First Name: {{firstName}}</h2>
+  <h2>Last Name: {{lastName}}</h2>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity';
 export default {
-    data () {
-        return {
-            name : "Evans"
-        }
-    }, 
-    mounted () {
+    setup () {
+        let first = ref('Evans');
+        let last = ref('Mutwiri');
+
         setTimeout(() => {
-            this.name = "Mutwiri";
+            first.value = 'Mutwiri',
+            last.value = 'Evans'
         }, 4000);
+        return {
+            firstName : first,
+            lastName : last
+        }
     }
 }
 </script>
 
 <style>
+body {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+}
 
+h1 {
+        /* font: 800 'Verdana'; */
+        font: 40px Georgia, serif;
+    }
+h2 {
+    font: 20px Arial, sans-serif;
+}
 </style>
